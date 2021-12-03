fb88786c35fc148dd2054e6e1486e793786a9b50
Jared Rooprai <hello@jaredrooprai.com>
2021-12-03 07:50:45 -0700

    remove angularls fix/ was fixed in plugin

--- nvim/lua/nvim-lsp-installer-config.lua
+++ nvim/lua/nvim-lsp-installer-config.lua
@@ -4,23 +4,6 @@ local lsp_installer = require("nvim-lsp-installer")
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
     local opts = {}
-
-    if server.name == 'angularls' then
-	    local cmd = {
-	      server.root_dir .. '/node_modules/.bin/ngserver',
-	      '--stdio',
-	      '--tsProbeLocations',
-	      server.root_dir,
-	      '--ngProbeLocations',
-	      server.root_dir .. '/node_modules/@angular/language-server'
-	    }
-
-	    opts.cmd = cmd
-	    opts.on_new_config = function(new_config)
-	      new_config.cmd = cmd
-	    end
-    end
-
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
     --     opts.root_dir = function() ... end
