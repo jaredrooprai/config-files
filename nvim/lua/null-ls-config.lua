24346b3de4f850c6f36acdaf9e80f0c8e4733f07
Jared Rooprai <hello@jaredrooprai.com>
2022-03-25 20:10:44 -0600

    add back null ls

--- nvim/lua/null-ls-config.lua
+++ nvim/lua/null-ls-config.lua
@@ -1,1 +1,16 @@
-require("null-ls").setup()
+require("null-ls").setup({
+	require("null-ls").setup({
+		sources = {
+			require("null-ls").builtins.formatting.google_java_format,
+			require("null-ls").builtins.formatting.stylua,
+			require("null-ls").builtins.formatting.prettierd,
+			require("null-ls").builtins.formatting.eslint_d,
+			require("null-ls").builtins.diagnostics.eslint_d,
+		},
+		on_attach = function(client)
+			if client.resolved_capabilities.document_formatting then
+				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			end
+		end,
+	}),
+})
