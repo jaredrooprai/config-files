e10a85d8effe200c59001cdd2c8e36a2f03c2eef
Jared Rooprai <hello@jaredrooprai.com>
2022-04-16 10:03:23 -0600

    format lua

--- nvim/lua/plugin-configs/startify-config.lua
+++ nvim/lua/plugin-configs/startify-config.lua
@@ -1,6 +1,8 @@
-vim.api.nvim_exec([[
-  let g:startify_bookmarks = ['~/config-files/', '~/Sites/showpass-frontend', '~/Sites/web-app']
-  let g:startify_change_to_vcs_root = 1
-  let g:startify_lists = [{'type': 'bookmarks', 'header': ['Bookmarks']}, {'type': 'files', 'header': ['Files']}]
-]], false);
-
+vim.api.nvim_exec(
+	[[
+    let g:startify_bookmarks = ['~/config-files/', '~/Sites/showpass-frontend', '~/Sites/web-app']
+    let g:startify_change_to_vcs_root = 1
+    let g:startify_lists = [{'type': 'bookmarks', 'header': ['Bookmarks']}, {'type': 'files', 'header': ['Files']}]
+  ]],
+	false
+)
