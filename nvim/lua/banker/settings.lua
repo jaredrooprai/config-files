6402912d603b3d6c07b8022bf00fcb802f7e18df
Jared Rooprai <hello@jaredrooprai.com>
2025-01-23 16:36:39 -0700

    update settings

--- nvim/lua/banker/settings.lua
+++ nvim/lua/banker/settings.lua
@@ -1,4 +1,3 @@
-vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.ignorecase = true
@@ -6,21 +5,11 @@ vim.opt.smartcase = true
 vim.opt.incsearch = true
 vim.opt.list = true
 vim.opt.hidden = true
-vim.g.bufferline = {
-	animation = false,
-}
 vim.opt.mouse = "a"
-vim.opt.nuw = 6
-vim.g.loaded_matchparen = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
+
+-- Disable unused built-in plugins for faster startup
 vim.g.loaded_tarPlugin = 1
-vim.g.loaded_man = 1
-vim.g.loaded_gzip = 1
 vim.g.loaded_zipPlugin = 1
-vim.g.loaded_shada_plugin = 1
-vim.g.loaded_spellfire_pugin = 1
-vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_remote_plugins = 1
+vim.g.loaded_gzip = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_man = 1
