88369f43b103ea15b76d95b039370d4923cae964
Jared Rooprai <hello@jaredrooprai.com>
2025-10-28 21:31:57 -0600

    add obsidian tweak

--- nvim/lua/banker/lazy/obsidian.lua
+++ nvim/lua/banker/lazy/obsidian.lua
@@ -21,12 +21,13 @@ if vim.g.vscode == nil then
                 name = "personal",
                 path = "/Users/jaredrooprai/Library/Mobile Documents/iCloud~md~obsidian/Documents/Shared"
             }},
+            disable_frontmatter = true,
+            note_frontmatter_func = function(note)
+                return nil -- This ensures no frontmatter is generated
+            end,
             follow_url_func = function(url)
                 -- Open the URL in the default web browser.
                 vim.fn.jobstart({"open", url}) -- Mac OS
-                -- vim.fn.jobstart({"xdg-open", url})  -- linux
-                -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
-                -- vim.ui.open(url) -- need Neovim 0.10.0+
             end
 
             -- see below for full list of options 👇
